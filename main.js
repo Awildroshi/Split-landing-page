93920049911fbde6f47bcb3eaebf5e9609d2258f
Awildroshi <151027830+Awildroshi@users.noreply.github.com>
2025-01-09 22:24:36 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const right= document.querySelector('.right')
 const container= document.querySelector('.container')
 
 
+
 left.addEventListener('mouseenter', ()=> container.classList.add('hover-left'))
 left.addEventListener('mouseleave', ()=> container.classList.remove('hover-left'))
 
